07e4b7e5e3d47585fc4d76da2f64b5a61c36ba36
bitcamp <bitcamp@bit>
2017-11-23 14:30:40 +0900

    test/dev:Add past files

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/common/Search.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/common/Search.java
@@ -1,21 +1,23 @@
 package com.model2.mvc.common;
 
 
-//==>리스트화면을 모델링(추상화/캡슐화)한 Bean 
 public class Search {
 	
 	///Field
-	private int currentPage;
+	private int curruntPage;
 	private String searchCondition;
 	private String searchKeyword;
 	private int pageSize;
-	//==> 리스트화면 currentPage에 해당하는 회원정보를 ROWNUM 사용 SELECT 위해 추가된 Field 
-	//==> UserMapper.xml 의 
-	//==> <select  id="getUserList"  parameterType="search"	resultMap="userSelectMap">
-	//==> 참조
-	private int endRowNum;
-	private int startRowNum;
+	private String order;
 	
+	public String getOrder() {
+		return order;
+	}
+
+	public void setOrder(String order) {
+		this.order = order;
+	}
+
 	///Constructor
 	public Search() {
 	}
@@ -29,10 +31,10 @@ public class Search {
 	}
 	
 	public int getCurrentPage() {
-		return currentPage;
+		return curruntPage;
 	}
-	public void setCurrentPage(int currentPage) {
-		this.currentPage = currentPage;
+	public void setCurrentPage(int curruntPage) {
+		this.curruntPage = curruntPage;
 	}
 
 	public String getSearchCondition() {
@@ -41,28 +43,17 @@ public class Search {
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
-	
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
 	}
-	
-	//==> Select Query 시 ROWNUM 마지막 값 
-	public int getEndRowNum() {
-		return getCurrentPage()*getPageSize();
-	}
-	//==> Select Query 시 ROWNUM 시작 값
-	public int getStartRowNum() {
-		return (getCurrentPage()-1)*getPageSize()+1;
-	}
 
 	@Override
 	public String toString() {
-		return "Search [currentPage=" + currentPage + ", searchCondition="
+		return "Search [curruntPage=" + curruntPage + ", searchCondition="
 				+ searchCondition + ", searchKeyword=" + searchKeyword
-				+ ", pageSize=" + pageSize + ", endRowNum=" + endRowNum
-				+ ", startRowNum=" + startRowNum + "]";
+				+ ", pageSize=" + pageSize + "]";
 	}
 }
